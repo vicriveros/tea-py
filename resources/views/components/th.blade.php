1d6ec0f45dc1136c4fe3afe567b8bb5e41c894ad
Victor Riveros <vdriveros@gmail.com>
2024-04-15 17:25:55 -0500

    edicion de componente th, ordenar datos de tabla de forma ascendente o descendente

--- resources/views/components/th.blade.php
+++ resources/views/components/th.blade.php
@@ -1,3 +1,31 @@
+@props([
+    'sortable' => false, 
+    'column' => null, 
+    'sortCol' => null, 
+    'sortAsc' => null
+    ])
+
 <th {!! $attributes->merge(['class' => 'px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-base uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left']) !!} >
-    {{ $text }}
+    @if($sortable)
+        <button wire:click="sortBy('{{ $column }}')" class="flex items-center gap-2">
+            {{ $slot }}
+
+                @if ($sortCol === $column)
+                    <div class="text-gray-400">
+                        @if ($sortAsc)
+                            <i class="fa-solid fa-sort-up"></i>
+                        @else
+                            <i class="fa-solid fa-sort-down"></i>
+                        @endif
+                    </div>
+                @else
+                    <div class="text-gray-400 ">
+                        <i class="fa-solid fa-sort"></i>
+                    </div>
+                @endif
+        </button> 
+    @else
+        {{ $slot }}
+    @endif
+
 </th>
