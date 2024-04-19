4561250a6aa3ebcaed38f8c49033fd3e0b8af61c
DerlisZR <dmzaracho@gmail.com>
2024-04-19 19:18:26 -0400

    Create 2024_04_12_124912_cambiar_propiedad_to_personas_table.php

---
+++ database/migrations/2024_04_12_124912_cambiar_propiedad_to_personas_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('personas', function (Blueprint $table) {
+            $table->date('fecha_nacimiento')->change();
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('personas', function (Blueprint $table) {
+            //
+        });
+    }
+};
