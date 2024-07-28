1ce7f3ae2d6efd4ba468824013ab35b129f2f78b
DerlisZR <dmzaracho@gmail.com>
2024-07-27 22:02:04 -0400

    Medicos, especialidad CORREGIDO

--- core/app/Livewire/Medico/Especialidad.php
+++ core/app/Livewire/Medico/Especialidad.php
@@ -2,8 +2,8 @@
 
 namespace App\Livewire\Medico;
 
-use App\Models\Especialidades;
 use App\Models\Medicos;
+use App\Models\Especialidades;
 use App\Models\MedicosEspecialidades;
 
 use Livewire\Component;
@@ -13,39 +13,38 @@ class Especialidad extends Component
     public Medicos $medico;
     public $medico_id = '';
     public $especialidad_id = '';
-        
+    
     public $message = false;
 
     public function mount(Medicos $medicoid){
         $this->medico = $medicoid;
+
         $this->medico_id = $this->medico->id;
     }
 
-    public function save_esp(){
-        $esp = Especialidades::find($this->especialidad_id);
-            MedicosEspecialidades::create(
-            $this->only(['especialidad_id', 'medico_id'])
+    public function save_espe(){
+
+        MedicosEspecialidades::create(
+            $this->only(['medico_id', 'especialidad_id'])
         );
 
         $this->message = true;
     }
 
-    public function delete_esp(MedicosEspecialidades $espe){
+    public function delete_espe(MedicosEspecialidades $espe){
         $espe->delete();
     }
 
-    public static function getEspecialidadName($id){
-        $esp = Especialidades::find($id);
-        return $esp->nombre;    
+    public static function getEspecialidadesName($id){
+        $espe = Especialidades::find($id);
+        return $espe->nombre;    
     }
 
     public function render()
     {
         return view('livewire.medico.especialidad', [
-            'me_espe' => MedicosEspecialidades::where('medico_id', $this->medico_id)
-            ->get(),
-            'especialidad' => Especialidades::all()
-
+            'me_especialidades' => MedicosEspecialidades::where('medico_id', $this->medico_id)->get(),
+            'especialidades' => Especialidades::all()
         ]);
     }
 }
